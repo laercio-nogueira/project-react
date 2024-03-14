ffd07f0caed95de0c6ec62f3e9e0f96730e4c84d
Laercio Nogueira <14927246+laercio-nogueira@users.noreply.github.com>
2024-03-14 19:38:18 -0300

    feat: format htmlElement

--- html-vs-react/html.js
+++ html-vs-react/html.js
@@ -19,29 +19,72 @@ class MyCustomElement extends HTMLElement {
         border-radius: 10px;
         cursor: pointer;
         box-shadow: 1px 1px 2px #999;
-        margin-top: 10px;
+      }
+
+      .container {
+        border: 1px dotted #999;
+        padding: 10px 0px;
+        margin: 10px 0px;
+      }
+
+      .row {
+        display: flex;
+        flex-direction: row;
       }
     `;
     return style
   }
 
-  render() {
-    this.innerHTML = `
-      <button type="button" class="button">
-        HTML Button
-      </button>
-    `
+  render(props, element) {
+    const values = {
+      props: props.props || props,
+      type: props.type
+    }
+    
+    const el = document.createElement(values.type)
 
-    this.appendChild(this.style());
+    if(typeof(values.props) === "string") {
+      element.innerText = values.props
+    } else {
+      Object.keys(values.props).forEach(key => {
+        if (key !== "children") el.setAttribute(key, values.props[key])
+      })
+
+      if (values.props.children) {
+        this.render(values.props.children, el)
+      }
+      
+      if (element) {
+        element.appendChild(el)
+      } else {
+        this.appendChild(el)
+        this.appendChild(this.style());
+      }
+    }
+    
   }
+
   connectedCallback() {
+    
+    const component = (
+      <div class="container" data-set="22" key="1">
+        <div class="row">
+          <button type="button" class="button">
+            HTML Button
+          </button>
+        </div>
+      </div>
+    )
+
     this.addEventListener('click', e => {
+      this.removeChild(this.childNodes[0])
+      this.removeChild(this.childNodes[0])
       this.state.background = "red"
-      this.render()
+      this.render(component)
       this.props.changeOption("HTML")
     });
 
-    this.render()
+    this.render(component)
   }
 }
 
